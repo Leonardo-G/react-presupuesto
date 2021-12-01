3c198c7dfb58608d545bd92ca7315a94fe0cf269
leonardog9823 <guanuco43@gmail.com>
2021-12-01 18:47:58 -0300

    obteniendo el presupuesto

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,13 +1,28 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { PresupuestoContext } from '../context/PresupuestoContext'
+import { Mensaje } from './Mensaje'
 
 export const NuevoPresupuesto = () => {
+    const { presupuesto, setPresupuesto } = useContext(PresupuestoContext)
+    const [mensaje, setMensaje] = useState("")
 
-    const { presupuesto } = useContext(PresupuestoContext)
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+
+        if(presupuesto === "" || presupuesto <= 0 || isNaN(Number(presupuesto)) ){
+            setMensaje("coloque un presupuesto válido")
+        }else{
+            console.log("Si es un presupuesto válido")
+        }
+
+    }
 
     return (
         <div className="contenedor-presupuesto contenedor sombra">
-            <form className="formulario">
+            <form 
+                className="formulario"
+                onSubmit={ handlePresupuesto }    
+            >
                 <div className="campo">
                     <label>Definir Presupuesto</label>
                     <input
@@ -15,6 +30,7 @@ export const NuevoPresupuesto = () => {
                         type="text"
                         placeholder="Añade un presupuesto"
                         value={ presupuesto }
+                        onChange={ e => setPresupuesto(e.target.value) }
                     />
                     <input
                         type="submit"
@@ -22,6 +38,7 @@ export const NuevoPresupuesto = () => {
                     />
 
                 </div>
+                { mensaje && <Mensaje tipo="error">{ mensaje }</Mensaje>}
             </form>
         </div>
     )
